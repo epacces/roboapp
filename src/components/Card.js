44adc1fc9b1dfab77abdfdefe0b8420f8b60094d
Eriprando Pacces <eriprando.pacces@gmail.com>
2020-08-29 18:25:41 +0100

    add hashing

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Hash from 'object-hash';
 
 const Card = (props) => {
     return (
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt='robots' src={`https://robohash.org/${props.id}?200x200&set=set${props.id%4+1}`} />
+            <img alt='robots' src={`https://robohash.org/${Hash(props)}?200x200&set=set${props.id%4+1}`} />
             <div>
                 <h2>{props.name}</h2>
                 <p>{props.email}</p>
