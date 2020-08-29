f05218c42db3e19eb9f7253e361437d6c5aba960
Eriprando Pacces <eriprando.pacces@gmail.com>
2020-08-29 14:18:22 +0100

    Updates

---
+++ precache-manifest.47a3fa7d8976881d422b02a2ea372b39.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c64c5de41128ef623cd0079a9e023fa",
+    "url": "/epacces/roboapp/index.html"
+  },
+  {
+    "revision": "2af9402d52b700f61d9d",
+    "url": "/epacces/roboapp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c819a7d6de94f583aecb",
+    "url": "/epacces/roboapp/static/css/main.d279f7b0.chunk.css"
+  },
+  {
+    "revision": "2af9402d52b700f61d9d",
+    "url": "/epacces/roboapp/static/js/2.aa3f3a9a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/epacces/roboapp/static/js/2.aa3f3a9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c819a7d6de94f583aecb",
+    "url": "/epacces/roboapp/static/js/main.bd453850.chunk.js"
+  },
+  {
+    "revision": "167c8ccae1f70213e328",
+    "url": "/epacces/roboapp/static/js/runtime-main.a16fa03d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/epacces/roboapp/static/media/SEGA.12a3db8b.woff"
+  }
+]);
